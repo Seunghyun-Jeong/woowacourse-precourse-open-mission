e04992192a283a4709ac86fec53b99d4ec6c7fc6
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-11-11 17:51:54 +0900

    refactor: MVC 구조로 리팩토링

--- src/main/kotlin/lotto/controller/LottoGameController.kt
+++ src/main/kotlin/lotto/controller/LottoGameController.kt
@@ -1,6 +1,7 @@
 package lotto.controller
 
 import lotto.service.LottoGameService
+import lotto.validate.MoneyValidator
 import lotto.view.LottoGameView
 
 class LottoGameController {
@@ -8,8 +9,24 @@ class LottoGameController {
     private val view = LottoGameView()
 
     fun execute() {
-        val purchaseMoney = service.inputPurchaseMoney()
-        val purchaseCount = purchaseMoney / LottoGameService.LOTTO_PRICE
-        view.printPurchaseCount(purchaseCount)
+        val purchaseMoney = inputPurchaseMoney()
     }
+
+    fun inputPurchaseMoney(): Int {
+        while (true) {
+            println("구입금액을 입력해 주세요.")
+            val input = readln()
+            try {
+                val purchaseMoney = input?.toInt() ?: throw NumberFormatException()
+                MoneyValidator(purchaseMoney)
+                println()
+                return purchaseMoney
+            } catch (e: NumberFormatException) {
+                println("[ERROR] 숫자만 입력해 주세요.")
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
 }
--- src/main/kotlin/lotto/service/LottoGameService.kt
+++ src/main/kotlin/lotto/service/LottoGameService.kt
@@ -1,27 +1,10 @@
 package lotto.service
 
-import lotto.validate.MoneyValidator
-import kotlin.NumberFormatException
+import lotto.view.LottoGameView
 
 class LottoGameService {
     companion object {
         const val LOTTO_PRICE = 1000
     }
 
-    fun inputPurchaseMoney(): Int {
-        while (true) {
-            println("구입금액을 입력해 주세요.")
-            val input = readln()
-            try {
-                val purchaseMoney = input?.toInt() ?: throw NumberFormatException()
-                MoneyValidator(purchaseMoney)
-                println()
-                return purchaseMoney
-            } catch (e: NumberFormatException) {
-                println("[ERROR] 숫자만 입력해 주세요.")
-            } catch (e: IllegalArgumentException) {
-                println(e.message)
-            }
-        }
-    }
 }
